9fd1b8c663370f077c5b8fd6e8a24fa69c254a3c
mahnoor-ismail01 <mahnoor@students.uit.edu>
2022-10-28 22:57:18 +0500

    correct value reg

--- generators/chipyard/src/main/scala/example/JustRead.scala
+++ generators/chipyard/src/main/scala/example/JustRead.scala
@@ -53,7 +53,7 @@ trait JustReadTopModule extends HasRegMap{
     val io:JustReadTopIO
     implicit val p: Parameters
     def params:JustReadParams
-    val isJustRead: Reg(UInt(params.width.W)) 
+    val isJustRead: RegInit(230.UInt(params.width.W)) 
     val impl = Module(new JustReadMMIOChiselModule(params.width))
     isJustRead:=impl.io.OutportToBeRead
     regmap(
