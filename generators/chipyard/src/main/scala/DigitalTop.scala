751f27be8faa2f65342674762a167294cd76b5b0
mahnoor-ismail01 <mahnoor@students.uit.edu>
2022-10-28 23:23:38 +0500

    n

--- generators/chipyard/src/main/scala/DigitalTop.scala
+++ generators/chipyard/src/main/scala/DigitalTop.scala
@@ -35,7 +35,6 @@ class DigitalTop(implicit p: Parameters) extends ChipyardSystem
   with fftgenerator.CanHavePeripheryFFT // Enables optionally having an MMIO-based FFT block
   with constellation.soc.CanHaveGlobalNoC // Support instantiating a global NoC interconnect
   with chipyard.example.CanHavePeripheryJustRead
-  
   {
   override lazy val module = new DigitalTopModule(this)
   }
